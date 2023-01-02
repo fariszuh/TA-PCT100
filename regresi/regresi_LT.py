3450134d787fcf8cdde0b422725e9d5fd25b0ff1
fariszuh <fariszuh@gmail.com>
2023-01-03 04:36:43 +0700

    adding comment # for scaling vLTfeedback-->level & otherwise for controller output signal

--- regresi/regresi_LT.py
+++ regresi/regresi_LT.py
@@ -3,17 +3,19 @@
 level_sebenarnya = [0,0] #inisiasi level
 voltOutput_LT = [0,0] #inisiasi volt output
 # data n=1, awal
-level_sebenarnya[0] = 4.5
-voltOutput_LT[0] = -0.1814297703517198
+level_sebenarnya[0] = 4.5 #cm
+voltOutput_LT[0] = -0.1814297703517198 #vdc
 # data n terakhir
-level_sebenarnya[1] = 20
-voltOutput_LT[1] = 8.906538490882735
-
+level_sebenarnya[1] = 20 #cm
+voltOutput_LT[1] = 8.906538490882735 #vdc
+# sinyal feedback LT, (volt LT ---> level)
 # perhitungan pers. garis lurus
 # m = (y2-y1) / (x2-x1)
 m = (level_sebenarnya[1] - level_sebenarnya[0])/(voltOutput_LT[1] - voltOutput_LT[0])
 # c = y1 - m*x1
 c = level_sebenarnya[0] - m*voltOutput_LT[0]
+
+# khusus inverse, sinyal kontrol kontroller (level --> voltage pump)
 mi= 1/m
 ci= -c/m
 print(mi)
