eb0aae57b6dd427764907e51e48f6c379f718e32
fariszuh <fariszuh@gmail.com>
2023-01-02 18:21:15 +0700

    changing desired tank level into 20cm, find m & c

--- regresi/regresi_LT.py
+++ regresi/regresi_LT.py
@@ -3,11 +3,11 @@
 level_sebenarnya = [0,0] #inisiasi level
 voltOutput_LT = [0,0] #inisiasi volt output
 # data n=1, awal
-level_sebenarnya[0] = 0
-voltOutput_LT[0] = -0.296177615014878
+level_sebenarnya[0] = 4.5
+voltOutput_LT[0] = -0.1814297703517198
 # data n terakhir
-level_sebenarnya[1] = 19.5
-voltOutput_LT[1] = 8.71061265
+level_sebenarnya[1] = 20
+voltOutput_LT[1] = 8.906538490882735
 
 # perhitungan pers. garis lurus
 # m = (y2-y1) / (x2-x1)
