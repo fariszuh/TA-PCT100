b8a2067d840e2ad8c774c476713d038fe3e2a585
fariszuh <fariszuh@gmail.com>
2023-01-27 21:24:12 +0700

    Solved to Subscribe Multiple Topics

--- MQTT/testMQTT.py
+++ MQTT/testMQTT.py
@@ -5,54 +5,54 @@ import time
 
 from paho.mqtt import client as mqtt_client
 
+
 broker = 'test.mosquitto.org'
 port = 1883
 topic = "PCT100/level"
-# generate client ID with pub prefix randomly
-# client_id = f'python-mqtt-{random.randint(0, 1000)}'
-# username = 'emqx'
-# password = 'public'
+SP = 10
+kp = 0
+ki = 0
+kd = 0
 
-# def connect_mqtt():
-#     def on_connect(client, userdata, flags, rc):
-#         if rc == 0:
-#             print("Connected to MQTT Broker!")
-#         else:
-#             print("Failed to connect, return code %d\n", rc)
+def on_message_SP(client, userdata, message):
+    global SP
+    SP = str(message.payload.decode())
+    # print(baca)
+    # print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
+def on_message_kp(client, userdata, message):
+    global kp
+    kp = str(message.payload.decode())
+def on_message_ki(client, userdata, message):
+    global ki
+    ki = str(message.payload.decode())
+def on_message_kd(client, userdata, message):
+    global kd
+    kd = str(message.payload.decode())
+
+# def MQTT_subscribe(alamatTopik):
+#     def on_message(client, userdata, message):
+#         global baca
+#         baca = str(message.payload.decode())
+#         # print(f"Received `{message.payload.decode()}` from `{message.topic}` topic")
 #
-#     # client = mqtt_client.Client(client_id)
-#     client = mqtt_client.Client(client_id)
-#     # client.username_pw_set(username, password)
-#     client.on_connect = on_connect
-#     client.connect(broker, port)
-#     return client
-
-# def publish(client):
-#     msg_count = 0
-#     while True:
-#         time.sleep(1)
-#         msg = f"messages: {msg_count}"
-#         result = client.publish(topic, msg)
-#         # result: [0, 1]
-#         status = result[0]
-#         if status == 0:
-#             print(f"Send `{msg}` to topic `{topic}`")
-#         else:
-#             print(f"Failed to send message to topic {topic}")
-#         msg_count += 1
-
-# def run():
-#     client = connect_mqtt()
-#     client.loop_start()
-#     publish(client)
-#     client.subscribe("TEMPERATURE")
+#
+#     client.subscribe(alamatTopik)
 #     client.on_message = on_message
 
-def on_message(client, userdata, message):
-    global SP
-    SP = str(message.payload.decode("utf-8"))
-    print("received message: " ,str(message.payload.decode("utf-8")))
-    # return SP
+    # def on_message(client, userdata, message):
+    #     global hasilBaca
+    #     hasilBaca = str(message.payload.decode("utf-8"))
+    #     # print(hasilBaca)
+    #     return hasilBaca
+    #     # print("received message: " ,str(message.payload.decode("utf-8")))
+    #     # return SP
+    #
+    # global hasilBaca
+    # # print(alamatTopik)
+    # client.subscribe(str(alamatTopik))
+    # client.on_message = on_message
+    # print(client)
+    # return hasilBaca
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
@@ -81,11 +81,30 @@ while True:
     else:
         print(f"Failed to send message to topic {topic}")
     msg_count += 1
-    client.subscribe("PCT100/SP")
-    client.on_message = on_message
 
-    bacaSP = on_message(client)
-    print("SP: " + str(bacaSP))
+    # ['PCT100/SP', 'PCT100/kp', 'PCT100/ki', 'PCT100/kd']
+    # [('PCT100/SP', 0), ('PCT100/kp', 0), ('PCT100/ki', 0), ('PCT100/kd', 0)]
+    client.subscribe('PCT100/SP')
+    client.on_message = on_message_SP
+    client.unsubscribe('PCT100/SP')
+    client.subscribe('PCT100/kp')
+    client.on_message = on_message_kp
+    client.unsubscribe('PCT100/kp')
+    client.subscribe('PCT100/ki')
+    client.on_message = on_message_ki
+    client.unsubscribe('PCT100/ki')
+    client.subscribe('PCT100/kd')
+    client.on_message = on_message_kd
+    client.unsubscribe('PCT100/kd')
+
+    print("SP : " + str(SP) + " , kp : " + str(kp) + " , ki : " + str(ki) + " , kd : " + str(kd))
+        # print(baca)
+
+    # inMSG = MQTT_subscribe("PCT100/SP")
+    # print("Masuk SP = " + str(inMSG))
+
+    # bacaSP = on_message(client)
+    # print("SP: " + str(bacaSP))
 
 
 # time.sleep(30)
--- test.py
+++ test.py
@@ -1,44 +1,53 @@
-import time
-import tkinter as tk
-import matplotlib
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+arr_k = [0]
+k=608
+arr_k.append(k)
+print(arr_k)
 
-import matplotlib.pyplot as plt
-matplotlib.use('TkAgg')
-start = time.time()
-arr_elapse = []
-arr_volt_level = []
-elapse_last = -1
-fig = Figure();
-ax = fig.add_subplot(111)
 
-#ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
-ax.set_title('Serial Data');
-ax.set_xlabel('Sample')
-ax.set_ylabel('Voltage')
-ax.set_xlim(0,100)
-ax.set_ylim(-0.5,6)
-lines = ax.plot([],[])[0]
-window = tk.Tk()
-window.title('GUI Open Loop PCT-100')
-window.configure(background = 'light blue')
-window.geometry("700x500")
-canvas = FigureCanvasTkAgg(fig, master=window)  # A tk.DrawingArea.
-canvas.get_tk_widget().place(x = 10,y=10, width = 500,height = 400)
 
-while True:
-    end = time.time()
-    elapse = int(end - start)
 
-    if not elapse == elapse_last:
-        arr_elapse.append(elapse)
-        volt_level = elapse + 1
-        arr_volt_level.append(volt_level)
-        lines.set_xdata(elapse)
-        lines.set_ydata(volt_level)
-        canvas.draw()
-        canvas.flush_events()
-        print(arr_elapse)
-        elapse_last = elapse
 
+# import time
+# import tkinter as tk
+# import matplotlib
+# from matplotlib.figure import Figure
+# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+#
+# import matplotlib.pyplot as plt
+# matplotlib.use('TkAgg')
+# start = time.time()
+# arr_elapse = []
+# arr_volt_level = []
+# elapse_last = -1
+# fig = Figure();
+# ax = fig.add_subplot(111)
+#
+# #ax = plt.axes(xlim=(0,100),ylim=(0, 120)); #displaying only 100 samples
+# ax.set_title('Serial Data');
+# ax.set_xlabel('Sample')
+# ax.set_ylabel('Voltage')
+# ax.set_xlim(0,100)
+# ax.set_ylim(-0.5,6)
+# lines = ax.plot([],[])[0]
+# window = tk.Tk()
+# window.title('GUI Open Loop PCT-100')
+# window.configure(background = 'light blue')
+# window.geometry("700x500")
+# canvas = FigureCanvasTkAgg(fig, master=window)  # A tk.DrawingArea.
+# canvas.get_tk_widget().place(x = 10,y=10, width = 500,height = 400)
+#
+# while True:
+#     end = time.time()
+#     elapse = int(end - start)
+#
+#     if not elapse == elapse_last:
+#         arr_elapse.append(elapse)
+#         volt_level = elapse + 1
+#         arr_volt_level.append(volt_level)
+#         lines.set_xdata(elapse)
+#         lines.set_ydata(volt_level)
+#         canvas.draw()
+#         canvas.flush_events()
+#         print(arr_elapse)
+#         elapse_last = elapse
+#
