f2f4b4f712029b867092987a63f5b0ae6b89cab2
Muhammad Faris Zuhairi <39437790+fariszuh@users.noreply.github.com>
2023-11-26 22:02:15 +0700

    add NN journal

---
+++ References for Citation/Faris's Journal Reference/Neural Network Journal/model_bab2.m
@@ -0,0 +1,39 @@
+clc; clear
+SP=10;
+A = 201.143;
+R = 22; % cm3/s
+% controller
+C_level = zpk([1],[2 1],1)
+C_flow = kp*e;
+% disturbance
+D_flow = 1*rand(1,1);
+D_level = 1*rand(1,1);
+% plant
+G_flow = 
+G_level = 
+% feedback transmitter
+H_FT = 
+H_LT = K_pompa*u_pompa
+
+Set the network has 𝐿 layers
+Set each layer has 𝑛𝑙 nodes
+For ℎ = 1 To 𝐿 − 2
+p1(h) = alpha*e-beta(h+2)
+For 𝑙1 = 1 To 𝐿 − 2
+For 𝑛𝑙 = 1 To 𝑛𝑙
+For 𝑛2 = 1 To 𝑛𝑙
+If rand < 𝑝
+wl1(l1+1)
+n1n2 = 0
+n3 = randint(1, nl)
+t = 1
+While 𝑡 = 1
+p2 = rand
+For 𝑙
+2 = L - 2 To 𝑙1 + 2
+If 𝑝2 < 𝑝1(𝑙2)
+bl1l2
+n1n3 = rand
+t = 0
+Break
+Finish!
